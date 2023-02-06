351dd7554569148dbd3272fe184c6ff513173d36
Minionguyjpro <66115754+Minionguyjpro@users.noreply.github.com>
2023-02-06 20:11:51 +0100

    Update WinUEFI-console.bat

--- bin/WinUEFI-console.bat
+++ bin/WinUEFI-console.bat
@@ -1,12 +1,14 @@
 @echo off
+set VERSION=${{ github.REF_NAME }}
+echo WinUEFI %VERSION% by FreakinSoftMania
 echo WinUEFI by FreakinSoftMania
 timeout /t 3 > nul
 echo Rebooting in 3...
-timeout /t 1 > nul
+timeout /nobreak /t 1 > nul
 echo Rebooting in 2...
-timeout /t 1 > nul
+timeout/nobreak /t 1 > nul
 echo Rebooting in 1...
-timeout /t 1> nul
+timeout /nobreak /t 1 > nul
 echo Rebooting...
-timeout /t 2 > nul
+timeout /nobreak /t 2 > nul
 shutdown /fw /f /r /t 00
